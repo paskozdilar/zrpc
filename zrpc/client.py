787db33d0c2c21d0cb3ee8b041fd870f47c4eeb0
paskozdilar <pasko.zdilar@checklens.ai>
2021-03-25 14:33:12 +0100

    Add initial client/server implementation

---
+++ zrpc/client.py
@@ -0,0 +1,126 @@
+"""
+Synchronous ZRPC client.
+
+Instantiate this class and use the `.call` method to call an RPC method.
+"""
+
+import logging
+import os
+import stat
+import time
+import uuid
+import zmq
+from zrpc.utils.exceptions import (
+        ConnectError,
+        RPCError,
+        RPCTimeoutError,
+)
+from zrpc.utils.serialization import serialize, deserialize
+
+
+logger = logging.getLogger(__name__)
+
+
+class Client:
+    def __init__(self, socket_dir='/tmp/zrpc_sockets/'):
+        socket_dir = os.path.abspath(socket_dir)
+
+        context = zmq.Context.instance()
+        sockets = {}
+
+        self._context = context
+        self._poller = zmq.Poller()
+        self._sockets = sockets
+        self._socket_dir = socket_dir
+
+        try:
+            os.makedirs(socket_dir, exist_ok=True)
+            for socket_name in os.listdir(socket_dir):
+                self.__connect(socket_name)
+        except (OSError, zmq.ZMQError) as exc:
+            raise ConnectError('Cannot connect client') from exc
+
+    def __connect(self, socket_name):
+        context = self._context
+        sockets = self._sockets
+        socket_dir = self._socket_dir
+
+        if socket_name in sockets:
+            raise RPCError('Service "%s" already connected' % socket_name)
+
+        socket_path = os.path.join(socket_dir, socket_name)
+        socket_mode = os.stat(socket_path).st_mode
+        if not stat.S_ISSOCK(socket_mode):
+            raise RPCError('Service "%s" unavailable' % socket_name)
+
+        socket = context.socket(zmq.REQ)
+        socket.connect('ipc://' + socket_path)
+        sockets[socket_name] = socket
+
+        self._poller.register(socket)
+        logger.debug('Connected to "%s"' % socket_name)
+
+    def __disconnect(self, socket_name):
+        sockets = self._sockets
+
+        if socket_name not in sockets:
+            raise RPCError('Service "%s" is not connected' % socket_name)
+
+        socket = sockets.pop(socket_name)
+        socket.close(linger=0)
+
+        self._poller.unregister(socket)
+        logger.debug('Disconnected from "%s"' % socket_name)
+
+    def call(self, service, method, payload, timeout=None):
+        """
+        Call an RPC method of a service with a payload.
+        If timeout is None, blocks indefinitely.
+        If timeout is a number, blocks `timeout` seconds and raises
+        RPCTimeoutError on timeout.
+        """
+        if timeout is None or timeout < 0:
+            timeout = float('inf')
+
+        sockets = self._sockets
+        if service not in sockets:
+            self.__connect(service)
+        socket = sockets[service]
+
+        request_id = str(uuid.uuid4())
+        request = serialize([request_id, method, payload])
+
+        start_time = time.monotonic()
+        events = {}
+
+        current_time = time.monotonic()
+
+        iter_timeout = 1
+        while (current_time - start_time) < timeout:
+            socket.send(request)
+            timeout_ms = 1000 * min(iter_timeout, timeout - (current_time - start_time))
+            events = dict(self._poller.poll(timeout=timeout_ms))
+
+            if socket in events:
+                break
+
+            logger.error('Service "%s" down - reconnecting...' % service)
+            self.__disconnect(service)
+            self.__connect(service)
+            socket = sockets[service]
+            iter_timeout += 1
+
+        if socket not in events:
+            raise RPCTimeoutError('Service "%s" not responding' % service)
+
+        response_data = socket.recv()
+        response = deserialize(response_data)
+        [response_id, payload, is_exception] = response
+
+        if request_id != response_id:
+            raise RPCError('Internal error (request ID does not match)')
+
+        if is_exception:
+            raise RPCError(payload)
+
+        return payload
---
+++ zrpc/server.py
@@ -0,0 +1,106 @@
+"""
+Synchronous ZRPC server.
+
+Subclass the `Server` class and use the `rpc_method` decorator to make the
+method available to ZRPC clients.
+"""
+
+import logging
+import os
+import re
+import zmq
+from zrpc.utils.exceptions import ConnectError
+from zrpc.utils.serialization import serialize, deserialize
+
+
+logger = logging.getLogger(__name__)
+
+
+class Server:
+    _rpc_methods: dict = None
+
+    def __init__(self, name=None, socket_dir='/tmp/zrpc_sockets'):
+        if name is None:
+            # Convert CamelCase class name into snake_case
+            class_name = self.__class__.__name__
+            name = re.sub('([A-Z]+)', r'_\1', class_name).strip('_').lower()
+            logger.warning('Service name not set -- using "%s".' % name)
+        socket_dir = os.path.abspath(socket_dir)
+
+        context = zmq.Context.instance()
+        socket = context.socket(zmq.REP)
+        socket_path = os.path.join(socket_dir, name)
+
+        try:
+            os.makedirs(socket_dir, exist_ok=True)
+            socket.bind('ipc://' + socket_path)
+        except (OSError, zmq.ZMQError) as exc:
+            raise ConnectError('Server init error') from exc
+
+        for method in self._rpc_methods.keys():
+            logger.info('Registered RPC method: "%s"' % method)
+
+        self._context = context
+        self._socket = socket
+
+    def run(self):
+        socket = self._socket
+        logger.info('Entering main loop...')
+
+        while True:
+            request_data = socket.recv()
+            logger.debug('NEW RPC REQUEST')
+
+            try:
+                request = deserialize(request_data)
+                [request_id, method_name, payload] = request
+                logger.debug('Decoded request: [%s, %s, %s]'
+                             % (request_id, method_name, str(payload)[:50]))
+            except (SerializationError, ValueError) as exc:
+                logger.error('Received malformed RPC request!')
+
+            try:
+                method = self._rpc_methods.get(method_name)
+                logger.debug('Executing "%s" with payload "%s"...'
+                             % (method_name, str(payload)[:50]))
+                payload = method(self, payload)
+                is_exception = False
+            except Exception as exc:
+                logger.error('--- RPC METHOD EXCEPTION ---', exc_info=True)
+                payload = '%s: %s' % (type(exc).__name__, exc)
+                is_exception = True
+
+            logger.debug('Serializing RPC response "%s"...' % str(payload)[:50])
+            response = [request_id, payload, is_exception]
+            response_data = serialize(response)
+            logger.debug('Sending RPC response "%s"...' % str(response_data)[:50])
+            socket.send(response_data)
+
+
+class __RPCDecoratorClass:
+    """
+    When we decorate a method with an ordinary function decorator, the
+    decorator receives the method (function object) as an argument.
+    However, the owner class cannot be determined from the function object
+    alone.
+
+    Somehow, __set_name__ method does exactly what we need:
+
+        https://stackoverflow.com/a/54316392/8713894
+
+    TODO:
+        Spend more time understanding the __set_name__ method and write better
+        documentation explaining this mechanism.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __set_name__(self, owner, name):
+        if owner._rpc_methods is None:
+            owner._rpc_methods = {}
+        owner._rpc_methods[name] = self.func
+        setattr(owner, name, self.func)
+
+
+rpc_method = __RPCDecoratorClass
